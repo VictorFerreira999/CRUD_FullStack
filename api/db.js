23ddb51153a73353b5e72a35bc4920ff3a2fb12a
VictorFerreira999 <victorferreira9990@gmail.com>
2024-07-26 12:46:50 -0300

    ok

--- api/db.js
+++ api/db.js
@@ -1,8 +1,16 @@
-import mysql from "mysql"
+import mysql from 'mysql2'; //
 
 export const db = mysql.createConnection({
-   host:"localhost",
-   user: "victor",
-   password: "123456789",
-   database: crud
-})
+  host: 'localhost',
+  user: 'victor',
+  password: '123456789',
+  database: 'crud'
+});
+
+db.connect((err) => {
+  if (err) {
+    console.error('Erro ao conectar ao banco de dados:', err);
+    return;
+  }
+  console.log('Conectado ao banco de dados.');
+});
--- api/index.js
+++ api/index.js
@@ -1,8 +1,15 @@
-import express from "express"
+import express from 'express';
+import userRoutes from './routes/users.js';
+import cors from 'cors';
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
-app.listen(8800)
+app.use('/', userRoutes);
+
+// Inicie o servidor HTTP
+app.listen(8800, () => {
+  console.log('Servidor HTTP rodando na porta 8800');
+});
